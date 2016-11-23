1b75d9e9068ee064de061d917864e0a31aa92db9
chasebrasfield <chasebrasfield@gmail.com>
2016-11-23 11:17:13 -0600

    Sign in component added

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,21 @@
 <template>
   <div id="app">
-    <Test></Test>
+    <!-- This component is being used to test out different features in Vue -->
+    <!-- <Test></Test> -->
+    <!-- Remove Test component before implememnting actual code -->
+    <SignIn />
   </div>
 </template>
 
 <script>
 import Test from './components/Hello.vue'
+import SignIn from './components/sign-in.vue'
 
 export default {
   name: 'app',
   components: {
-    Test
+    Test,
+    SignIn
   }
 }
 </script>
---
+++ src/components/sign-in.vue
@@ -0,0 +1,58 @@
+<template>
+  <div id="sign-in">
+    <h1>Sign in page</h1>
+    <input type="input" name="userName" value="">
+    <button type="button" name="signInButton">Sign In</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'sign-in',
+  data () {
+    return {}
+  }
+}
+</script>
+
+<style lang='scss' scoped>
+#sign-in {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+.container {
+  width: 1000px;
+  margin: auto;
+  border-left: 2px solid #2c3e50;
+  min-height: 200px;
+  text-align: left;
+  padding-left: 50px;
+}
+
+.button {
+  margin: 10px;
+}
+
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
