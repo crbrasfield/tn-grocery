010b744a3db43d92ad5fddb260b233435449c5c2
chasebrasfield <chasebrasfield@gmail.com>
2016-11-23 11:01:24 -0600

    Added test component to test out Vue features

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,16 @@
 <template>
   <div id="app">
-    <hello></hello>
+    <Test></Test>
   </div>
 </template>
 
 <script>
-import Hello from './components/Hello.vue'
+import Test from './components/Hello.vue'
 
 export default {
   name: 'app',
   components: {
-    Hello
+    Test
   }
 }
 </script>
--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,43 +1,63 @@
 <template>
-  <div id="hello">
-    <img src="http://vuejs.org/images/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vueify" target="_blank">vueify</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+  <div id="test">
+    <div class="container">
+      <h1>{{ msg }}</h1>
+    </div>
+    <div class="container">
+      Type your message here:
+      <input v-model="alertText"></input>
+      <button class="button" type="button" name="button" @click="alertMe">
+        Alert your message
+      </button>
+      <br>
+      Your message: {{ alertText }}
+    </div>
+    <button type="button" name="button" @click="logTest">Click me to log test</button>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'hello',
+  name: 'test',
   data () {
     return {
-      msg: 'Welcome to Your TNEST GROCERY App'
+      msg: 'Welcome to the Test component. This is where you can test out the features offered by Vue and get the hang of data-binding, event handeling, and component structure!',
+      derp: 'test',
+      alertText: null
+    }
+  },
+  methods: {
+    logTest () {
+      console.log('test')
+    },
+    alertMe () {
+      alert(this.alertText)
     }
   }
 }
 </script>
 
 <style scoped>
-#hello {
+#test {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.container {
+  width: 1000px;
+  margin: auto;
+  border-left: 2px solid #2c3e50;
+  min-height: 200px;
+  text-align: left;
+  padding-left: 50px;
+}
+
+.button {
+  margin: 10px;
 }
 
 h1, h2 {
