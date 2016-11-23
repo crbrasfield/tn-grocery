1fd48787444d4cbded7d0c0c4bb45a1af40a926c
chasebrasfield <chasebrasfield@gmail.com>
2016-11-23 11:09:07 -0600

    Added SASS

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -37,7 +37,7 @@ export default {
 }
 </script>
 
-<style scoped>
+<style lang='scss' scoped>
 #test {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
