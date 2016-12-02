d9eedbb5c4c87133d9f8c5f9697ee0691b7325b4
Eric Hubbard <eric@theranest.com>
2016-12-02 16:51:17 -0600

    Reset password component created
    resetting password through email is now possible

--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import VueRouter from 'vue-router'
 import SignIn from './components/sign-in.vue'
 import SignUp from './components/sign-up.vue'
+import ResetPassword from './components/reset-password.vue'
 import Dashboard from './components/dashboard.vue'
 import UserInfoPanel from './components/user-info-panel.vue'
 import NotFound from './components/not-found.vue'
@@ -9,8 +10,9 @@ import PostItem from './components/post-item.vue'
 const routes = [
   { path: '/sign-in', component: SignIn },
   { path: '/sign-up', component: SignUp },
+  { path: '/reset-password', component: ResetPassword },
   { path: '/', component: Dashboard },
-  { path: '*', component: NotFound }
+  { path: '*', component: NotFound },
 
 ]
 
