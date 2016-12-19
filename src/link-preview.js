6994f0642eac5264fbf5efb58daacb6b780d3089
chasebrasfield <chasebrasfield@gmail.com>
2016-12-19 17:07:40 -0600

    removed test line

--- src/link-preview.js
+++ src/link-preview.js
@@ -5,7 +5,7 @@ export const key = '5852c7276adede1c5990c959b00181fbc30bf49c3d3f2'
 Vue.use(VueResource)
 
 export const linkPreview = (url) => {
-  return Vue.http.get(`http://api.linkpreview.net/?key=5852c7276adede1c5990c959b00181fbc30bf49c3d3f2&q=${url}`).then((response, err) => {
+  return Vue.http.jsonp(`https://api.linkpreview.net/?key=5852c7276adede1c5990c959b00181fbc30bf49c3d3f2&q=${url}`).then((response, err) => {
     return response
   }, (err) => {
     console.log('err:', err)
